dbed80709f572e8bd33b9cc4cb9eead77e3dbe41
Macarez Anthony <la238361@student.helha.be>
2025-06-28 11:05:48 +0200

    Correction de Google Maps

--- src/components/contact/MapSection.tsx
+++ src/components/contact/MapSection.tsx
@@ -11,7 +11,7 @@ const MapSection = () => (
     <CardContent>
       <div className="h-64 rounded-lg overflow-hidden mb-4">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.289896374582!2d3.2086744!3d50.7455833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2f6b0b0b0b0b1%3A0x1234567890abcdef!2sRue%20des%20Pr%C3%A9s%2084B%2C%207700%20Mouscron%2C%20Belgium!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.289896374582!2d3.2086744!3d50.7455833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2f6b0b0b0b0b1%3A0x1234567890abcdef!2sRue%20des%20Pr%C3%A9s%2084B%2C%207700%20Mouscron%2C%20Belgium!5e0!3m2!1sfr!2sus!4v1704067200000!5m2!1sfr!2sus"
           width="100%"
           height="100%"
           style={{ border: 0 }}
